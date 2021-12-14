56bc745e1c00b61c7a872e0cd1a37ab1a99c2034
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-12-13 17:13:55 -0800

    feat: constructor sets `org`, `number`, `octokit`, (custom) `fields`

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,27 @@
-export default class GitHubProject {
-  constructor();
+import { Octokit } from "@octokit/core";
+
+export type READ_ONLY_FIELDS = {
+  title: "Title";
+  assignees: "Assignees";
+  labels: "Labels";
+  repository: "Repository";
+  milestone: "Milestone";
+};
+
+export default class GitHubProject<
+  TFields extends Record<string, string> = {}
+> {
+  org: string;
+  number: number;
+  octokit: Octokit;
+  fields: READ_ONLY_FIELDS & TFields;
+  constructor(options: GitHubProjectOptions<TFields>);
 }
+
+export type GitHubProjectOptions<TFields extends Record<string, string> = {}> =
+  {
+    org: string;
+    number: number;
+    octokit: Octokit;
+    fields?: TFields;
+  };
--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+// @ts-check
+
+/** @type {import("./").READ_ONLY_FIELDS} */
+export const READ_ONLY_FIELDS = {
+  title: "Title",
+  assignees: "Assignees",
+  labels: "Labels",
+  repository: "Repository",
+  milestone: "Milestone",
+};
+
 export default class GitHubProject {
-  constructor(options) {}
+  org;
+  number;
+  octokit;
+  fields;
+
+  /**
+   * @param {import(".").GitHubProjectOptions} options
+   */
+  constructor({ org, number, octokit, fields = {} }) {
+    this.org = org;
+    this.number = number;
+    this.octokit = octokit;
+    this.fields = {
+      ...fields,
+      ...READ_ONLY_FIELDS,
+    };
+  }
 }
