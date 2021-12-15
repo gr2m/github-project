85c52c4b3600f7b5f5362cbe5ff6385664fca455
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-12-14 16:15:46 -0800

    fix: project.items.add() adding existing item after project.items.list() does not send query to add item again

--- api/items.add.js
+++ api/items.add.js
@@ -15,32 +15,50 @@ import { getFieldsUpdateQuery } from "./lib/get-fields-update-query.js";
  * @returns {Promise<import("..").GitHubProjectItem>}
  */
 export default async function addItem(project, state, contentNodeId, fields) {
+  let newOrExistingItem;
+  if (state.didLoadItems) {
+    const existingItem = state.items.find(
+      // @ts-expect-error - does not handle the conditional chaining operator
+      (item) => item.issueOrPullRequest?.id === contentNodeId
+    );
+
+    if (existingItem && !fields) existingItem;
+    newOrExistingItem = existingItem;
+  }
+
   const stateWithFields = await getStateWithProjectFields(project, state);
 
-  const {
-    addProjectNextItem: { projectNextItem },
-  } = await project.octokit.graphql(addIssueToProjectMutation, {
-    projectId: stateWithFields.id,
-    contentId: contentNodeId,
-  });
+  if (!newOrExistingItem) {
+    const {
+      addProjectNextItem: { projectNextItem },
+    } = await project.octokit.graphql(addIssueToProjectMutation, {
+      projectId: stateWithFields.id,
+      contentId: contentNodeId,
+    });
 
-  const newItem = projectItemNodeToGitHubProjectItem(
-    stateWithFields,
-    projectNextItem
-  );
+    newOrExistingItem = projectItemNodeToGitHubProjectItem(
+      stateWithFields,
+      projectNextItem
+    );
 
-  if (!fields) return newItem;
+    // add newly created item to cache
+    if (state.didLoadItems) {
+      state.items.push(newOrExistingItem);
+    }
+  }
+
+  if (!fields) return newOrExistingItem;
 
   const query = getFieldsUpdateQuery(stateWithFields, fields);
   await project.octokit.graphql(query, {
     projectId: stateWithFields.id,
-    itemId: newItem.id,
+    itemId: newOrExistingItem.id,
   });
 
   return {
-    ...newItem,
+    ...newOrExistingItem,
     fields: {
-      ...newItem.fields,
+      ...newOrExistingItem.fields,
       ...fields,
     },
   };
