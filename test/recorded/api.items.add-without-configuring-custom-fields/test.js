aaa0ba8fbe0bdb49b64749a01eda136683cf6eee
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2022-07-12 15:59:55 -0700

    test/recorded/api.items.add-without-configuring-custom-fields/

---
+++ test/recorded/api.items.add-without-configuring-custom-fields/test.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+import GitHubProject from "../../../index.js";
+
+/**
+ * @param {import("../../../").default} testProject
+ * @param {string} itemId
+ */
+export function test(testProject, itemId = "PNI_1") {
+  const project = new GitHubProject({
+    org: testProject.org,
+    number: testProject.number,
+    octokit: testProject.octokit,
+    fields: {},
+  });
+
+  return project.items.add(itemId);
+}
