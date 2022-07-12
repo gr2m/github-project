73302732096254128c91f74ae716ec2ff4dc35fc
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2022-07-12 16:46:53 -0700

    test/recorded/api.items.add-existing-item-after-api.items.list/

---
+++ test/recorded/api.items.add-existing-item-after-api.items.list/test.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+/**
+ * @param {import("../../..").default} project
+ * @param {string} itemId
+ */
+export async function test(project, itemId = "I_1") {
+  await project.items.list();
+  return project.items.add(itemId);
+}
