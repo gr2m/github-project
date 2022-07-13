655e4643d091d9d7fc5f425203b1941afe49ceaf
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2022-07-13 14:42:41 -0700

    test/recorded/api.items.update-by-content-id/

---
+++ test/recorded/api.items.update-by-content-id/test.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * @param {import("../../../").default} project
+ * @param {string} [contentId]
+ */
+export function test(project, contentId = "I_1") {
+  return project.items.updateByContentId(contentId, { text: "new text" });
+}
