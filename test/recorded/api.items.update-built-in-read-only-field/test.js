5abc35f887ddaa05270d555a597b1d459aa6e596
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2022-07-13 13:58:05 -0700

    test/recorded/api.items.update-built-in-read-only-field/

---
+++ test/recorded/api.items.update-built-in-read-only-field/test.js
@@ -0,0 +1,25 @@
+// @ts-check
+
+import GitHubProject from "../../../index.js";
+
+/**
+ * @param {import("../../../").default} testProject
+ * @param {string} itemId
+ */
+export function test(testProject, itemId = "PNI_1") {
+  const project = new GitHubProject({
+    org: testProject.org,
+    number: testProject.number,
+    octokit: testProject.octokit,
+    fields: {
+      assignees: "Assignees",
+    },
+  });
+
+  return project.items.update(itemId, { assignees: "something" }).then(
+    () => {
+      throw new Error("Should not resolve");
+    },
+    (error) => error
+  );
+}
